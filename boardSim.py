20051fb6277aef8107f1245a1044a8433ab06104
Cam <66324329+terpyPy@users.noreply.github.com>
2022-06-24 23:06:37 -0400

    Delete boardSim.py

--- boardSim.py
+++
@@ -1,9 +0,0 @@
-import random
-import boardFunc
-from matplotlib import pyplot as plt
-theBoard = [[0]*4 for _ in range(4)]
-
-for i in range(16):
-    boardFunc.gameLogic(theBoard,random.randrange(0,16),1,0)
-    plt.imshow(theBoard)
-    plt.show()
